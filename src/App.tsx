8eb7d590864777d97e49471331f46991fb39956e
wuchuheng <root@wuchuheng.com>
2022-01-09 19:51:51 +0800

    fix: fix errors caused by file path.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import RouteList from "./routes";
+import Routes  from "./routes";
 import {BrowserRouter} from "react-router-dom";
 
 function App() {
 
     return (
         <BrowserRouter>
-            <RouteList />
+            <Routes />
         </BrowserRouter>
         )
 }
